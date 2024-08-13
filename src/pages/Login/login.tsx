9bf65b5fcdfa1ca823c89bfcdf97612bd81c017d
Wellington Albino <well.renato@hotmail.com>
2024-08-13 20:13:56 -0300

    Finalizando task_02

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -13,25 +13,25 @@ export function Login() {
 
   const { usuario, handleLogin } = useContext(AuthContext);
 
-  const {isLoading} = useContext(AuthContext) 
+  const { isLoading } = useContext(AuthContext)
 
   useEffect(() => {
-    if (usuario && usuario.token !== "") {
-        navigate('/home')
+    if (usuario.token !== "") {
+      navigate('/home')
     }
-}, [usuario])
+  }, [usuario])
 
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-  setUsuarioLogin({
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuarioLogin({
       ...usuarioLogin,
       [e.target.name]: e.target.value
-  })
-}
+    })
+  }
 
-function login(e: ChangeEvent<HTMLFormElement>) {
-  e.preventDefault()
-  handleLogin(usuarioLogin)
-}
+  function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+    handleLogin(usuarioLogin)
+  }
 
   return (
     <div className="flex flex-col items-center w-[600px] bg-white py-16 my-24 mx-auto rounded-3xl shadow-custom">
@@ -40,39 +40,39 @@ function login(e: ChangeEvent<HTMLFormElement>) {
       <form className="mt-16 space-y-10 flex flex-col items-center" onSubmit={login}>
         <div className="flex flex-col gap-3">
           <label htmlFor="email" className="text-4xl font-bold">Seu e-mail:</label>
-          <input className="bg-zinc-100 w-[400px] h-16 text-3xl px-2 rounded-2xl" 
-          type="email"
-          id="usuario"
-          name="usuario"
-          placeholder="E-mail"
-          value={usuarioLogin.usuario}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
+          <input className="bg-zinc-100 w-[400px] h-16 text-3xl px-2 rounded-2xl"
+            type="email"
+            id="usuario"
+            name="usuario"
+            placeholder="E-mail"
+            value={usuarioLogin.usuario}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
         </div>
 
         <div className="flex flex-col gap-3">
           <label htmlFor="senha" className="text-4xl font-bold">Sua senha:</label>
-          <input className="bg-zinc-100 w-[400px] h-16 text-3xl px-2 rounded-2xl" 
-          type="password"
-          id="senha" 
-          name="senha"
-          placeholder="Senha"
-          value={usuarioLogin.senha}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}/>
+          <input className="bg-zinc-100 w-[400px] h-16 text-3xl px-2 rounded-2xl"
+            type="password"
+            id="senha"
+            name="senha"
+            placeholder="Senha"
+            value={usuarioLogin.senha}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
         </div>
 
         <button className="w-44 h-16 bg-rose-500 text-2xl font-bold rounded-3xl text-rose-50 hover:bg-red-700 hover:scale-110 transition-all" type="submit">
           {isLoading ? <RotatingLines
-          strokeColor="white"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="24"
-          visible={true}
-          /> : 
-          
-          <span>ENTRAR</span>}
+            strokeColor="white"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="24"
+            visible={true}
+          /> :
+
+            <span>ENTRAR</span>}
         </button>
 
-        <hr className="border-slate-800 w-full"/>
+        <hr className="border-slate-800 w-full" />
 
         <span className="text-center text-xl font-bold">
           <p>Ainda não tem um cadastro?</p>
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://projetointegrador-coracaosolidario.onrender.com/'
 })
 
 
