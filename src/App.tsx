32bcb9728b4c100a3adfb7c5f487b86994ed3ae3
Camila Vildoso <camiliun123@gmail.com>
2024-08-14 17:52:00 -0300

    atualizar responsividad login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { About } from "./pages/About/About";
 import { Contact } from "./pages/Contact/Contact";
 import { Home } from "./pages/Home/Home";
@@ -15,6 +15,7 @@ export function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<DefaultLayout />}>
+             <Route index element={<Navigate to="/home" replace />} />
               <Route path="/home" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/info" element={<Info />} />
--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -34,13 +34,13 @@ export function Login() {
   }
 
   return (
-    <div className="flex flex-col items-center w-[450px] bg-white py-6 my-[50px] mx-auto rounded-3xl shadow-custom">
-      <h1 className="text-[25px] font-bold border-b-8 pb-1 border-b-[#F43F5E] rounded-lg">Faça seu login</h1>
+    <div className="flex flex-col items-center  w-[340px]  md:w-[450px]   bg-white py-6 my-[50px] mx-auto rounded-3xl shadow-custom">
+      <h1 className="text-[23px] md:text-[25px] font-bold border-b-8 pb-1 border-b-[#F43F5E] rounded-lg">Faça seu login</h1>
 
       <form className="mt-[40px] space-y-6 flex flex-col items-center" onSubmit={login}>
         <div className="flex flex-col gap-1">
-          <label htmlFor="email" className="text-[18px] font-bold">Seu e-mail:</label>
-          <input className="bg-[#e5e5e5] w-[300px] h-10 text-[18px] px-2 rounded-2xl"
+          <label htmlFor="email" className="text-[16px] md:text-[18px] font-bold">Seu e-mail:</label>
+          <input className="bg-[#e5e5e5] w-[300px] h-10 text-[16px] md:text-[18px] px-2 rounded-2xl"
             type="email"
             id="usuario"
             name="usuario"
@@ -50,8 +50,8 @@ export function Login() {
         </div>
 
         <div className="flex flex-col gap-1">
-          <label htmlFor="senha" className="text-[18px] font-bold">Sua senha:</label>
-          <input className="bg-[#e5e5e5] w-[300px] h-10 text-[18px] px-2 rounded-2xl"
+          <label htmlFor="senha" className="text-[16px] md:text-[18px] font-bold">Sua senha:</label>
+          <input className="bg-[#e5e5e5] w-[300px] h-10 text-[16px] md:text-[18px] px-2 rounded-2xl"
             type="password"
             id="senha"
             name="senha"
@@ -60,7 +60,7 @@ export function Login() {
             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
         </div>
 
-        <button className="w-[120px] h-10 bg-rose-500 text-[18px] font-bold rounded-3xl text-rose-50 hover:bg-red-700 hover:scale-110 transition-all" type="submit">
+        <button className="w-[120px] h-10 bg-rose-500 text-[16px] md:text-[18px] font-bold rounded-3xl text-rose-50 hover:bg-red-700 hover:scale-110 transition-all" type="submit">
           {isLoading ? <RotatingLines
             strokeColor="white"
             strokeWidth="5"
@@ -74,7 +74,7 @@ export function Login() {
 
         <hr className="border-slate-800 w-full" />
 
-        <span className="text-center text-[16px] font-bold">
+        <span className="text-center text-[14px] md:text-[16px] font-bold">
           <p>Ainda não tem um cadastro?</p>
           <Link className="text-rose-500 hover:text-red-700 hover:underline transition-all" to="/register">Cadastre-se aqui!</Link>
         </span>
