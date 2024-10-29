7810455c052fe8d5d4cccf852353185d042afba0
Wellington Albino <well.renato@hotmail.com>
2024-10-29 15:51:13 -0300

    Trocando base URL

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL
-    baseURL: 'http://localhost:8080/'
+    baseURL: import.meta.env.VITE_API_URL
+    // baseURL: 'http://localhost:8080/'
 })
 
 
